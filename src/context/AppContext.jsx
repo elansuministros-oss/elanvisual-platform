36c32a07f9a13e7fe3dcbb81ed9c771c5abfd7b8
Erick Cano <elansuministros@gmail.com>
2026-06-13 23:35:29 -0600

    SEPARAR ESTADO ELANVISUAL DE ELANPET

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -184,7 +184,7 @@ function guardarStorage(clave, valor) {
 }
 
 const APP_STATE_TABLE = 'elanpet_app_state';
-const APP_STATE_ID = 'global';
+const APP_STATE_ID = 'elanvisual-v2';
 
 function construirEstadoCompartido({
   configuracion,
