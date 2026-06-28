117c2e45875f594cbc02f1c750a2a288fd7e9522
Erick Cano <elansuministros@gmail.com>
2026-06-27 22:32:47 -0600

    AI-19 limpia portada printy

--- src/printy/PrintyApp.jsx
+++ src/printy/PrintyApp.jsx
@@ -1,16 +1,10 @@
 ﻿import "./styles/printy.css";
 import "./styles/header.css";
 import "./styles/hero.css";
-import "./styles/categorias.css";
-import "./styles/productos.css";
-import "./styles/tracking.css";
 import "./styles/footer.css";
 
 import PrintyHeader from "./PrintyHeader";
 import PrintyHero from "./PrintyHero";
-import PrintyCategorias from "./PrintyCategorias";
-import PrintyProductos from "./PrintyProductos";
-import PrintyTracking from "./PrintyTracking";
 import PrintyFooter from "./PrintyFooter";
 
 export default function PrintyApp() {
@@ -18,9 +12,7 @@ export default function PrintyApp() {
     <main className="printy-app">
       <PrintyHeader />
       <PrintyHero />
-      <PrintyCategorias />
-      <PrintyProductos />
-      <PrintyTracking />
+      <section className="printy-empty-space" />
       <PrintyFooter />
     </main>
   );
