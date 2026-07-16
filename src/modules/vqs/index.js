1f86801fbce878ba0022fdba7c8634700818e907
elansuministros-oss <elansuministros@gmail.com>
2026-07-15 18:01:31 -0600

    VQS-01 exporta adapter de ELANVISUAL

--- src/modules/vqs/index.js
+++ src/modules/vqs/index.js
@@ -1,2 +1,3 @@
 export { createQuotationDocument, validateQuotationDocument, VQS_SCHEMA_VERSION } from './contracts/quotationDocument';
 export { elanvisualBrand, getActivePaymentAccounts } from './config/elanvisualBrand';
+export { adaptElanvisualQuote } from './adapters/elanvisualQuoteAdapter';
