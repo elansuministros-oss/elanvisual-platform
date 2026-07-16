55ea339571acc9c7e0743b84785264ea877365fc
elansuministros-oss <elansuministros@gmail.com>
2026-07-15 18:00:05 -0600

    VQS-01 publica API interna del módulo

---
+++ src/modules/vqs/index.js
@@ -0,0 +1,2 @@
+export { createQuotationDocument, validateQuotationDocument, VQS_SCHEMA_VERSION } from './contracts/quotationDocument';
+export { elanvisualBrand, getActivePaymentAccounts } from './config/elanvisualBrand';
